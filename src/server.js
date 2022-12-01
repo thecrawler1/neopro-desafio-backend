2414641b36236abdf01621a00bdfa0bd77e91fdf
Gabriel Alves <gabrielalves@outlook.com>
2022-12-01 18:03:03 -0300

    feat: create the error handler middleware

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import 'dotenv/config';
+import 'express-async-errors';
 
 const app = express();
-
 const port = process.env.PORT || 3001;
+
+// Error handler
+app.use((err, _req, res, _next) => {
+  res.status(err.statusCode || 500).json({ message: err.message });
+});
 
 app.listen(port, () => console.log(`Running on port ${port}`));
